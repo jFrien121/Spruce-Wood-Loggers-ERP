3f0b75dfe90006ec613b6bd0caac020ac2f83f50
Jocelyn Friesen <jocelynfriesen45@gmail.com>
2026-07-01 20:38:19 -0500

    Initial project solution commit

---
+++ Spruce-Wood-Loggers-ERP/Spruce-Wood-Loggers-ERP/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Spruce_Wood_Loggers_ERP
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
